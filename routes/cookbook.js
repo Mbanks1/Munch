358859d4bc37e162c5f5ec8603554522155aa64d
Mbanks1 <michael1banks@icloud.com>
2021-01-15 16:12:26 -0400

    cook book router

---
+++ routes/cookbook.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const CookCtrl = require("../controllers/cookbook");
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+
+module.exports = router;
